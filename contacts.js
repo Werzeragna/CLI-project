fbfae83f27d0ae1d33452d1be1269b0d5994c958
Maksym Lepikhov <Werzeragna@icloud.com>
2023-06-06 14:09:02 +0300

    Contacts refactoring with async/await

--- contacts.js
+++ contacts.js
@@ -2,55 +2,54 @@ const fs = require("fs").promises;
 const path = require("path");
 const contactsPath = path.join(__dirname, "/db/contacts.json");
 
-function listContacts() {
-  fs.readFile(contactsPath, "utf-8")
-    .then((data) => console.table(JSON.parse(data)))
-    .catch((err) => console.log(err.message));
+async function listContacts() {
+  try {
+    const data = await fs.readFile(contactsPath, "utf-8");
+    console.table(JSON.parse(data));
+  } catch (err) {
+    console.log(err.message);
+  }
 }
 
-function getContactById(contactId) {
-  fs.readFile(contactsPath, "utf-8")
-    .then((data) => JSON.parse(data))
-    .then((contacts) => contacts.filter((contact) => contact.id === contactId))
-    .then((contact) => console.table(contact))
-    .catch((err) => console.log(err.message));
+async function getContactById(contactId) {
+  try {
+    const data = await fs.readFile(contactsPath, "utf-8");
+    const contacts = JSON.parse(data);
+    const contact = contacts.filter((contact) => contact.id === contactId);
+    console.table(contact);
+  } catch (err) {
+    console.log(err.message);
+  }
 }
 
-function removeContact(contactId) {
-  fs.readFile(contactsPath, "utf-8")
-    .then((data) => JSON.parse(data))
-    .then((contacts) => contacts.filter((contact) => contact.id !== contactId))
-    .then((contact) => {
-      console.table(contact);
-      return contact;
-    })
-    .then((contact) => {
-      fs.writeFile(contactsPath, JSON.stringify(contact, null, "\t"), "utf-8");
-    })
-    .catch((err) => console.log(err.message));
+async function removeContact(contactId) {
+  try {
+    const data = await fs.readFile(contactsPath, "utf-8");
+    const contacts = JSON.parse(data);
+    const updatedContacts = contacts.filter((contact) => contact.id !== contactId);
+    console.table(updatedContacts);
+    await fs.writeFile(contactsPath, JSON.stringify(updatedContacts, null, "\t"), "utf-8");
+  } catch (err) {
+    console.log(err.message);
+  }
 }
 
-function addContact(name, email, phone) {
-  fs.readFile(contactsPath, "utf-8")
-    .then((data) => JSON.parse(data))
-    .then((contacts) => {
-      const contact = {
-        id: (contacts.length + 1).toString(),
-        name,
-        email,
-        phone,
-      };
-      contacts.push(contact);
-      return contacts;
-    })
-    .then((addContact) => {
-      console.table(addContact);
-      return addContact;
-    })
-    .then((contacts) => {
-      fs.writeFile(contactsPath, JSON.stringify(contacts, null, "\t"), "utf-8");
-    })
-    .catch((err) => console.log(err.message));
+async function addContact(name, email, phone) {
+  try {
+    const data = await fs.readFile(contactsPath, "utf-8");
+    const contacts = JSON.parse(data);
+    const contact = {
+      id: (contacts.length + 1).toString(),
+      name,
+      email,
+      phone,
+    };
+    contacts.push(contact);
+    console.table(contacts);
+    await fs.writeFile(contactsPath, JSON.stringify(contacts, null, "\t"), "utf-8");
+  } catch (err) {
+    console.log(err.message);
+  }
 }
 
 module.exports = { listContacts, getContactById, removeContact, addContact };
